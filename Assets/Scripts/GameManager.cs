1adc5bce95739de4f8862e4f45814c2859abac5f
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-02-28 12:11:23 +0100

    Implement new namespaces

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,8 @@ public class GameManager : MonoBehaviour
 	#region Variables
 	private static GameManager instance = null; // Instance of this object. or rather, this instance.
 	[SerializeField] private GameObject playerObject = default; // Reference to the player GameObject.
-	[SerializeField] private PlayerBehaviour playerBehaviour = default; // Reference to the PlayerBehaviour.
-	[SerializeField] private PlayerCameraBehaviour playerCameraBehaviour = default; // Reference to the PlayerCameraBehaviour.
+	[SerializeField] private Player player = default; // Reference to the PlayerBehaviour.
+	[SerializeField] private PlayerCamera playerCamera = default; // Reference to the PlayerCameraBehaviour.
 	[Space]
 	[SerializeField] private KeyCode pauseMenuKey = KeyCode.Escape; // Which key you have to press to open the pause menu.
 	[SerializeField] private GameObject pauseMenuObject = default;  // The Pausemenu.
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
 	#region Getters & Setters
 	public static GameManager Instance { get => instance; set => instance = value; }
 	public GameObject PlayerObject { get => playerObject; set => playerObject = value; }
-	public PlayerBehaviour PlayerBehaviour { get => playerBehaviour; set => playerBehaviour = value; }
+	public Player Player { get => player; set => player = value; }
 	#endregion
 
 	private void Awake()
@@ -41,8 +41,8 @@ public class GameManager : MonoBehaviour
 	void GetComponents()
 	{
 		playerObject = GameObject.FindGameObjectWithTag("Player");
-		playerBehaviour = playerObject.GetComponent<PlayerBehaviour>();
-		playerCameraBehaviour = playerObject.GetComponentInChildren<PlayerCameraBehaviour>();
+		player = playerObject.GetComponent<Player>();
+		playerCamera = playerObject.GetComponentInChildren<PlayerCamera>();
 	}
 
 	#region Menu Functions
@@ -52,7 +52,7 @@ public class GameManager : MonoBehaviour
 
 		Time.timeScale = pauseMenuEnableb ? 0f : 1f;
 		pauseMenuObject.SetActive(pauseMenuEnableb);
-		playerCameraBehaviour.LockCursor(pauseMenuEnableb);
+		playerCamera.LockCursor(pauseMenuEnableb);
 	}
 	#endregion
 }
