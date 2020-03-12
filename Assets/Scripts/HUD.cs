3a8bd3aa02d1264e8b5644c6013e1d245a7875b7
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-12 14:50:39 +0100

    Add HUD.cs

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+	#region Variables
+	[SerializeField] private float hudUpdateInterval = 0.1f;                // How often per second the HUD should update.
+	[Space(15)]
+	[SerializeField] private TextMeshProUGUI ammoTextMesh = default;        // Reference to the Ammo Text Mesh.
+	#endregion
+
+	#region Getters and Setters
+	public TextMeshProUGUI AmmoTextMesh { get => ammoTextMesh; set => ammoTextMesh = value; }
+	public float HudUpdateInterval { get => hudUpdateInterval; set => hudUpdateInterval = value; }
+	#endregion
+}
