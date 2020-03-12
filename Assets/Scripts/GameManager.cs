aec444eb6c76833d25ec456b3f6b9ca8fd83472a
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-12 14:52:19 +0100

    Add Functionality
    Add Function to get weapons on the player and the active weapon. Also update the HUD with the current weapon ammo amount

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,22 +8,25 @@ public class GameManager : MonoBehaviour
 	#region Variables
 	private static GameManager instance = null; // Instance of this object. or rather, this instance.
 
+	[SerializeField] private HUD hud = default;     // Reference to the HUD component.
+	[Space]
 	[SerializeField] private GameObject playerObject = default; // Reference to the player GameObject.
 	[SerializeField] private Player player = default; // Reference to the PlayerBehaviour.
 	[SerializeField] private PlayerCamera playerCamera = default; // Reference to the PlayerCameraBehaviour.
+	[SerializeField] private Transform playerWeaponTransform = default; // Reference to the PlayerWeaponTransform.
+	[SerializeField] private List<Gun> playerWeapons = default; // List with all the Weapons the player has picked up.
+	[SerializeField] private Gun playerActiveWeapon = default;  // Reference to the active and equiped weapon.
 	[Space]
 	[SerializeField] private KeyCode pauseMenuKey = KeyCode.Escape; // Which key you have to press to open the pause menu.
 	[SerializeField] private GameObject pauseMenuObject = default;  // The Pausemenu.
 	[SerializeField] private bool pauseMenuEnableb = false; // Bool to keep track of the state of the pause menu.
-	[Space]
-	[SerializeField] private TextMeshProUGUI ammoText = default;	// Referene to the Ammo TextMesh object.
 	#endregion
 
 	#region Getters & Setters
 	public static GameManager Instance { get => instance; set => instance = value; }
 	public GameObject PlayerObject { get => playerObject; set => playerObject = value; }
 	public Player Player { get => player; set => player = value; }
-	public TextMeshProUGUI AmmoText { get => ammoText; set => ammoText = value; }
+	public HUD Hud { get => hud; set => hud = value; }
 	#endregion
 
 	private void Awake()
@@ -35,6 +38,9 @@ public class GameManager : MonoBehaviour
 	private void Start()
 	{
 		GetComponents();
+		GetWeapons();
+		StartCoroutine(UpdateHUD());
+		StartCoroutine(GetActiveWeapon());
 	}
 
 	private void Update()
@@ -43,16 +49,49 @@ public class GameManager : MonoBehaviour
 			TriggerPauseMenu();
 	}
 
-	void GetComponents()
+	private void GetComponents()
 	{
 		playerObject = GameObject.FindGameObjectWithTag("Player");
 		player = playerObject.GetComponent<Player>();
 		playerCamera = playerObject.GetComponentInChildren<PlayerCamera>();
 	}
 
-	void GetWeapons()
+	public void GetWeapons()
 	{
+		for(int i = 0; i < playerWeaponTransform.childCount; i++)
+		{
+			if(playerWeaponTransform.GetChild(i).GetComponent<Gun>() && !playerWeapons.Contains(playerWeaponTransform.GetChild(i).GetComponent<Gun>()))
+			{
+				playerWeapons.Add(playerWeaponTransform.GetChild(i).GetComponent<Gun>());
+			}
+		}
+	}
 
+	private IEnumerator GetActiveWeapon()
+	{
+		while(true)
+		{
+			for(int i = 0; i < playerWeapons.Count; i++)
+			{
+				if(playerWeapons[i].GunState == GunState.Equiped)
+				{
+					playerActiveWeapon = playerWeapons[i];
+					break;
+				}
+			}
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+
+	private IEnumerator UpdateHUD()
+	{
+		while(true)
+		{
+			if(hud.AmmoTextMesh)
+				hud.AmmoTextMesh.text = playerActiveWeapon.CurrentAmmo + " / " + playerActiveWeapon.MaxAmmo;
+
+			yield return new WaitForSeconds(hud.HudUpdateInterval);
+		}
 	}
 
 	#region Menu Functions
