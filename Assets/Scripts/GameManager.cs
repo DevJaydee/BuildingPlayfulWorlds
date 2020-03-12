e1c656f2edd17d934962c85c9242629269803428
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-12 13:12:52 +0100

    Make start with getting current weapons and displaying ammo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 	#region Variables
 	private static GameManager instance = null; // Instance of this object. or rather, this instance.
+
 	[SerializeField] private GameObject playerObject = default; // Reference to the player GameObject.
 	[SerializeField] private Player player = default; // Reference to the PlayerBehaviour.
 	[SerializeField] private PlayerCamera playerCamera = default; // Reference to the PlayerCameraBehaviour.
@@ -13,12 +15,15 @@ public class GameManager : MonoBehaviour
 	[SerializeField] private KeyCode pauseMenuKey = KeyCode.Escape; // Which key you have to press to open the pause menu.
 	[SerializeField] private GameObject pauseMenuObject = default;  // The Pausemenu.
 	[SerializeField] private bool pauseMenuEnableb = false; // Bool to keep track of the state of the pause menu.
+	[Space]
+	[SerializeField] private TextMeshProUGUI ammoText = default;	// Referene to the Ammo TextMesh object.
 	#endregion
 
 	#region Getters & Setters
 	public static GameManager Instance { get => instance; set => instance = value; }
 	public GameObject PlayerObject { get => playerObject; set => playerObject = value; }
 	public Player Player { get => player; set => player = value; }
+	public TextMeshProUGUI AmmoText { get => ammoText; set => ammoText = value; }
 	#endregion
 
 	private void Awake()
@@ -43,6 +48,11 @@ public class GameManager : MonoBehaviour
 		playerObject = GameObject.FindGameObjectWithTag("Player");
 		player = playerObject.GetComponent<Player>();
 		playerCamera = playerObject.GetComponentInChildren<PlayerCamera>();
+	}
+
+	void GetWeapons()
+	{
+
 	}
 
 	#region Menu Functions
