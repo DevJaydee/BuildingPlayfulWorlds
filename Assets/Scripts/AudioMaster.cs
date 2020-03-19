ac6a79fd0efefc05e2d3450dfc79a86a0e672953
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-19 13:14:56 +0100

    Add AudioMaster
    This Script will handle all the audio. This way I don't have to pout audio clips on every single weapon, player and enemy. But I can just call it from an static instance

---
+++ Assets/Scripts/AudioMaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMaster : MonoBehaviour
+{
+	#region Variables
+	private static AudioMaster instance = null; // Instance of this
+	[SerializeField] private AudioClip[] groundWalkAudioClips = default;    // Array with all the ground walk audio clips.
+
+	public static AudioMaster Instance { get => instance; set => instance = value; }
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Awake()
+	{
+		if(!instance || instance != this)
+			instance = this;
+	}
+	#endregion
+
+	#region Public Voids
+	public void PlayWalkSound(AudioSource source)
+	{
+		int i = Random.Range(0, groundWalkAudioClips.Length);
+		source.clip = groundWalkAudioClips[i];
+	}
+	#endregion
+}
