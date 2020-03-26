ec430d93d856c911b6201294e79af6d017b4d050
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-26 13:11:19 +0100

    Add New ScriptableFloat object

---
+++ Assets/Scripts/ScriptableObjects/ScriptableFloat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableFloat", menuName = "ScriptableObjects/New Scriptable Float")]
+public class ScriptableFloat : ScriptableObject
+{
+	[SerializeField] private new string name;
+	[SerializeField] private float startingValue;
+	[SerializeField] private float value;
+
+	public string Name { get => name; set => name = value; }
+	public float StartingValue { get => startingValue; set => startingValue = value; }
+	public float Value { get => value; set => this.value = value; }
+
+	private void Awake()
+	{
+	}
+
+	private void OnEnable()
+	{
+		value = startingValue;
+	}
+}
