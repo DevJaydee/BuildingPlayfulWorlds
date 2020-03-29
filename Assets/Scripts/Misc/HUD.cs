250d8c72f7adb5bb9ff6645a63efd529ea09f65a
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-29 14:36:40 +0200

    Add New HUD Elements

--- Assets/Scripts/Misc/HUD.cs
+++ Assets/Scripts/Misc/HUD.cs
@@ -7,12 +7,13 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour
 {
 	#region Variables
-	[SerializeField] private float hudUpdateInterval = 0.1f;						// How often per second the HUD should update.
+	[SerializeField] private float hudUpdateInterval = 0.1f;                        // How often per second the HUD should update.
 	[Space(15)]
-	[SerializeField] private TextMeshProUGUI ammoTextMesh = default;				// Reference to the Ammo Text Mesh.
+	[Header("Player HUD Elements")]
+	[SerializeField] private TextMeshProUGUI ammoTextMesh = default;                // Reference to the Ammo Text Mesh.
 	[Space]
-	[SerializeField] private TextMeshProUGUI grenadeTextMesh = default;				// Reference to the Ammo Text Mesh.
-	[SerializeField] private ScriptableFloat grenadeAmount = default;				// Reference to the grenade amount float.
+	[SerializeField] private TextMeshProUGUI grenadeTextMesh = default;             // Reference to the Ammo Text Mesh.
+	[SerializeField] private ScriptableFloat grenadeAmount = default;               // Reference to the grenade amount float.
 	[Space]
 	[SerializeField] private Image playerHealthImage = default;                     // Reference to the Player Health Image Component.
 	[SerializeField] private ScriptableFloat playerHealthAmount = default;          // Reference to the player health amount float.
@@ -20,6 +21,15 @@ public class HUD : MonoBehaviour
 	[SerializeField] private Image playerStaminaImage = default;                    // Reference to the Player Stamina Image Component.
 	[SerializeField] private ScriptableFloat playerStaminaAmount = default;         // Reference to the player stamina amount float.
 
+	[Header("Enemy HUD Elements")]
+	[SerializeField] private TextMeshProUGUI currentEnemiesAliveTextMesh = default; // Reference to the Zombies alive text mesh.
+	[SerializeField] private ScriptableFloat currentEnemiesAliveAmount = default;   // Reference to the Zombies Alive Scriptablefloat.
+	[Space]
+	[SerializeField] private TextMeshProUGUI currentEnemyWaveTextMesh = default;  // Reference to the Zombie Wave text mesh.
+	[SerializeField] private ScriptableFloat currentEnemyWaveAmount = default;  // Reference to the current enemy wave Scriptablefloat.
+	[Space]
+	[SerializeField] private TextMeshProUGUI currentEnemyHealthMultiplierTextMesh = default;  // Reference to the Zombies health multiplier text mesh.
+	[SerializeField] private ScriptableFloat currentEnemyHealthMultiplierAmount = default;  // Reference to the current enemy health mulitplier Scriptablefloat.
 	#endregion
 
 	#region Getters and Setters
@@ -39,6 +49,10 @@ public class HUD : MonoBehaviour
 			grenadeTextMesh.text = grenadeAmount.Value.ToString();
 			playerHealthImage.fillAmount = playerHealthAmount.Value / 100;
 			playerStaminaImage.fillAmount = playerStaminaAmount.Value / 100;
+
+			currentEnemiesAliveTextMesh.text = "Zombies Alive: " + currentEnemiesAliveAmount.Value;
+			currentEnemyWaveTextMesh.text = "Current Wave: " + currentEnemyWaveAmount.Value;
+			currentEnemyHealthMultiplierTextMesh.text = "Health Multiplier: " + currentEnemyHealthMultiplierAmount.Value;
 
 			yield return new WaitForSeconds(hudUpdateInterval);
 		}
